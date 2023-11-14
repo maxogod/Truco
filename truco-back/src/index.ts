1542328b364ed9ed0f1d973725ead1e4493ec0c4
MelnikAndres <104472929+MelnikAndres@users.noreply.github.com>
2023-11-13 21:04:43 -0300

    Update: Index
    Split index.ts into app.ts and index.ts, for future testing.

--- truco-back/src/index.ts
+++ truco-back/src/index.ts
@@ -1,13 +1,4 @@
-import express from "express";
-import path from "path";
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-})
+import app from "./app";
 
 app.listen(8080, () => {
     console.log('App listening on port 8080');
