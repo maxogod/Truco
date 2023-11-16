68fda3a4e641f77f5ae21e70b1d80dff86abc3a6
maxogod <maxi18wars@gmail.com>
2023-11-16 12:19:52 -0300

    Connected to MongoAtlas db & added a .env with sensitive data

--- truco-back/src/app.ts
+++ truco-back/src/app.ts
@@ -1,9 +1,27 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import path from "path";
+import dotenv from "dotenv";
+import cors from "cors";
 
+dotenv.config();
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json()); // for parsing json (and create req.body etc)
+app.use(cookieParser()); // for parsing cookies
+app.use(express.urlencoded({ extended: true })); // for parsing URL-encoded request bodies
+
+if (process.env.DEBUG === "true") { // for development
+    app.use(
+        cors({
+            origin: "http://localhost:3001", // allow cors from :3001
+            credentials: true,
+        })
+    );
+    console.log("CORS enabled");
+}
+
+app.use(express.static(path.join(__dirname, 'public'))); // for serving static files
 
 app.use("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
