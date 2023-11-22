5eb18021192613f76d9a96057382eb1124d0a78e
Jorge <jorgesedek@gmail.com>
2023-11-22 19:10:07 -0300

    [feature] added deck shuffle

--- truco-front/src/components/Card.tsx
+++ truco-front/src/components/Card.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
  
 const MAX_VALUE = 12;
+const MAX_ID = 48;
 
 const suits = ['gold', 'cup', 'sword', 'club']
 const values: number[] = []
+const ids: number[] = []
 
 for (let newValue: number = 1; newValue <= MAX_VALUE; newValue += 1) {
   values.push(newValue); 
 }
+
+for (let newId: number = 1; newId <= MAX_ID; newId += 1) {
+    ids.push(newId); 
+  }
 
 console.log(values)
 
@@ -23,11 +29,15 @@ const Card = (props) => {
    const getDeck = () => {
     
     const deck: object[] = [];
-  
+    
+    let j = 0;
+
     for (let i = 0; i < suits.length; i++) {
       for (let x = 0; x < values.length; x++) {
-        const card = { Value: values[x], Suit: suits[i] };
+        
+        const card = { Id: ids[j], Value: values[x], Suit: suits[i] };
         deck.push(card);
+        j += 1;
       }
     }
     
@@ -36,5 +46,14 @@ const Card = (props) => {
   
   console.log(getDeck())
   
+  const shuffle = (deck: object[]) => { 
+    for (let i = deck.length - 1; i > 0; i--) { 
+      const j = Math.floor(Math.random() * (i + 1)); 
+      [deck[i], deck[j]] = [deck[j], deck[i]]; 
+    } 
+    return deck; 
+  }; 
+
+  console.log(shuffle(getDeck()))
   
-  export default Card
+ export default Card
