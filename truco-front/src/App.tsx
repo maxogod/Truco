1c0e8431ddfc7b3ac381b04f7b60a04a0eb2ba0d
@AndreasK24 <@Andreas24>
2023-11-25 11:39:05 -0300

    Added Timer

--- truco-front/src/App.tsx
+++ truco-front/src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import connectChannel from "./utils/connectChannel";
 import { getChannelName } from "./utils/pusherNames";
 import Channel from "pusher-js/types/src/core/channels/channel";
+import Timer from "./components/Timer"
 
 function App() {
   const [channel, setChannel] = useState<Channel | null>(null);
@@ -12,6 +13,7 @@ function App() {
   return (
     <div>
       <PusherUsageExample channel={channel}/>
+      <Timer />
     </div>
   )
 }
