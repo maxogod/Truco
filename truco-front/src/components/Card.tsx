6d2ee120bd1e28f5ca441556abd2eca9207fe71c
Jorge <jorgesedek@gmail.com>
2023-11-24 22:22:47 -0300

    [feature] changed object type to Card

--- truco-front/src/components/Card.tsx
+++ truco-front/src/components/Card.tsx
@@ -3,7 +3,7 @@ import Card from '../@types/card.ts'
 
 const MAX_Number = 12;
 
-const suits = ['sword', 'club', 'gold', 'cup']
+const suits = ['espada', 'basto', 'oro', 'copa']
 const numbers: number[] = []
 
 
@@ -14,10 +14,6 @@ const numbers: number[] = []
 for (let newNumber: number = 1; newNumber <= MAX_Number; newNumber += 1) {
   numbers.push(newNumber); 
 }
-
-
-
-console.log(numbers)
 
 const card = (number:number, suit: string) => {return {number, suit}};
 
@@ -34,10 +30,8 @@ export default function Card(props) {
 
 const getDeck = () => {
     
-  const deck: object[] = [];
+  const deck: Card[] = [];
     
-   
-
   for (let i = 0; i < suits.length; i++) {
     for (let x = 0; x < numbers.length; x++) {
         
@@ -54,34 +48,34 @@ const getDeck = () => {
   const cardEqual = (card:Card, otherCard:Card) => 
   {return (card.suit == otherCard.suit && card.number == otherCard.number)}
 
-  console.log(cardEqual(card(1,"sword"),card(1,"sword")))
+  console.log(cardEqual(card(1,"espada"),card(1,"espada")))
   
   const weights:object[] = [
-  [card(1,"sword")], 
-  [card(1,"club")], 
-  [card(7,"sword")],
-  [card(7,"gold")],
-  [card(3,"sword"), card(3,"club"), card(3,"gold"), card(3,"cup")],
-  [card(2,"sword"), card(2,"club"), card(2,"gold"), card(2,"cup")],
-  [card(1, "club"), card(1, "gold")],
-  [card(12,"sword"), card(12,"club"), card(12,"gold"), card(12,"cup")],
-  [card(11,"sword"), card(11,"club"), card(11,"gold"), card(11,"cup")],
-  [card(10,"sword"), card(10,"club"), card(10,"gold"), card(10,"cup")],
-  [card(7, "cup"), card(7, "club")],
-  [card(6,"sword"), card(6,"club"), card(6,"gold"), card(6,"cup")],
-  [card(5,"sword"), card(5,"club"), card(5,"gold"), card(5,"cup")],
-  [card(4,"sword"), card(4,"club"), card(4,"gold"), card(4,"cup")],
+  [card(1,"espada")], 
+  [card(1,"basto")], 
+  [card(7,"espada")],
+  [card(7,"oro")],
+  [card(3,"espada"), card(3,"basto"), card(3,"oro"), card(3,"copa")],
+  [card(2,"espada"), card(2,"basto"), card(2,"oro"), card(2,"copa")],
+  [card(1, "basto"), card(1, "oro")],
+  [card(12,"espada"), card(12,"basto"), card(12,"oro"), card(12,"copa")],
+  [card(11,"espada"), card(11,"basto"), card(11,"oro"), card(11,"copa")],
+  [card(10,"espada"), card(10,"basto"), card(10,"oro"), card(10,"copa")],
+  [card(7, "copa"), card(7, "basto")],
+  [card(6,"espada"), card(6,"basto"), card(6,"oro"), card(6,"copa")],
+  [card(5,"espada"), card(5,"basto"), card(5,"oro"), card(5,"copa")],
+  [card(4,"espada"), card(4,"basto"), card(4,"oro"), card(4,"copa")],
 ];
 
 const weight = (card:Card) => weights.findIndex(deck => deck.some((someCard:Card) => cardEqual(card, someCard)))
 
-console.log(weight(card(4,"gold")))
+console.log(weight(card(4,"oro")))
 
 
 
 console.log(getDeck())
   
-const shuffle = (deck: object[]) => { 
+const shuffle = (deck: Card[]) => { 
 for (let i = deck.length - 1; i > 0; i--) { 
     const j = Math.floor(Math.random() * (i + 1)); 
     [deck[i], deck[j]] = [deck[j], deck[i]]; 
@@ -89,6 +83,25 @@ for (let i = deck.length - 1; i > 0; i--) {
   return deck; 
   }; 
 
-  console.log(shuffle(getDeck()))
+const draw = (numberOfCardtoDraw: number, deck: Card[]) => {
+  const cardsDrawed:Card[] = [];
+  for(let i = 0; i < numberOfCardtoDraw; i++){
+    cardsDrawed.push(deck.pop() as Card);
+  }
+  return cardsDrawed;
   
- 
+}
+
+const deck = shuffle(getDeck());
+console.log(deck.length)
+
+console.log(draw(1,deck));
+console.log(deck.length)
+console.log(draw(3,deck));
+console.log(deck.length)
+console.log(draw(2,deck));
+console.log(deck.length)
+
+
+console.log(shuffle(getDeck()))
+  
