0c5fb00ac3977762d6a70572ff90bcd64bdb6dce
AndreasK24 <andreas24kuballa@gmail.com>
2023-11-29 01:32:16 -0300

    Added Score logic and style

--- truco-front/src/components/Score.tsx
+++ truco-front/src/components/Score.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Score: React.FC = () => {
-  // Implement your component logic here
+  const [teamAScore, setTeamAScore] = useState(0);
+    const [teamBScore, setTeamBScore] = useState(0);
+
+    const handleScoreChange = (team, amount) => {
+      if (team === 'A') {
+        setTeamAScore((prevScore) => prevScore + amount);
+      } else if (team === 'B') {
+        setTeamBScore((prevScore) => prevScore + amount);
+      }
+    };
 
   return (
-    <div className='w-[120px] h-[190px] rounded-xl bg-background text-white shadow-card flex justify-center rotate-6 absolute right-10'>
-      <h3 className='my-4'>You | He</h3>
+    <div className='w-[250px] h-[190px] rounded-xl bg-background text-white shadow-card flex justify-center rotate-6 right-10'>
+              <div className='flex flex-col justify-center'>
+                <h2>You</h2>
+                <p>Score: {teamAScore}</p>
+                <div >
+                  <button onClick={() => handleScoreChange('A', 1)}>Increment</button>
+                  <button onClick={() => handleScoreChange('A', -1)}>Decrement</button>
+                </div>
+              </div>
+              <div className='flex flex-col justify-center'>
+                <h2>He</h2>
+                <p>Score: {teamBScore}</p>
+                <div >
+                  <button onClick={() => handleScoreChange('B', 1)}>Increment</button>
+                  <button onClick={() => handleScoreChange('B', -1)}>Decrement</button>
+                </div>
+              </div>
     </div>
   );
 };
+
 
 export default Score;
