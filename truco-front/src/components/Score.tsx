9b03af6e2620a83cacc4f34c11defb6bea26d346
alenmonti <amonti@fi.uba.ar>
2023-11-29 13:38:07 -0300

    Change style and no negative points

--- truco-front/src/components/Score.tsx
+++ truco-front/src/components/Score.tsx
@@ -6,30 +6,30 @@ const Score: React.FC = () => {
 
     const handleScoreChange = (team, amount) => {
       if (team === 'A') {
-        setTeamAScore((prevScore) => prevScore + amount);
+        (teamAScore + amount >= 0) && setTeamAScore((prevScore) => prevScore + amount);
       } else if (team === 'B') {
-        setTeamBScore((prevScore) => prevScore + amount);
+        (teamAScore + amount >= 0) && setTeamBScore((prevScore) => prevScore + amount);
       }
     };
 
   return (
-    <div className='w-[250px] h-[190px] rounded-xl bg-background text-white shadow-card flex justify-center rotate-6 right-10'>
-              <div className='flex flex-col justify-center'>
-                <h2>You</h2>
-                <p>Score: {teamAScore}</p>
-                <div >
-                  <button onClick={() => handleScoreChange('A', 1)}>Increment</button>
-                  <button onClick={() => handleScoreChange('A', -1)}>Decrement</button>
-                </div>
-              </div>
-              <div className='flex flex-col justify-center'>
-                <h2>He</h2>
-                <p>Score: {teamBScore}</p>
-                <div >
-                  <button onClick={() => handleScoreChange('B', 1)}>Increment</button>
-                  <button onClick={() => handleScoreChange('B', -1)}>Decrement</button>
-                </div>
-              </div>
+    <div className='w-[120px] h-[190px] rounded-xl bg-background text-white shadow-card flex justify-evenly rotate-6 absolute right-10'>
+      <div className='flex flex-col justify-evenly'>
+        <h2>You</h2>
+        <p>{teamAScore}</p>
+        <div>
+          <button className='mr-2' onClick={() => handleScoreChange('A', 1)}>+</button>
+          <button onClick={() => handleScoreChange('A', -1)}>-</button>
+        </div>
+      </div>
+      <div className='flex flex-col justify-evenly'>
+        <h2>He</h2>
+        <p>{teamBScore}</p>
+        <div >
+          <button className='mr-2' onClick={() => handleScoreChange('B', 1)}>+</button>
+          <button onClick={() => handleScoreChange('B', -1)}>-</button>
+        </div>
+      </div>
     </div>
   );
 };
