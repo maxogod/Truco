0b7d1e2353819f6a049a936a88eb41a14d1d1580
maxogod <maxi18wars@gmail.com>
2023-11-30 16:13:40 -0300

    added requests logger middleware

--- truco-back/src/app.ts
+++ truco-back/src/app.ts
@@ -4,6 +4,7 @@ import path from "path";
 import dotenv from "dotenv";
 import cors from "cors";
 import pusherRouter from "./routers/pusherRouter";
+import requestLogger from "./middlewares/requestLogger";
 
 dotenv.config();
 const app = express();
@@ -23,6 +24,8 @@ if (process.env.DEBUG === "true") { // for development
 }
 
 app.use(express.static(path.join(__dirname, 'public'))); // for serving static files
+
+app.use(requestLogger); // for logging requests and status codes
 
 app.use("/api/pusher", pusherRouter);
 
