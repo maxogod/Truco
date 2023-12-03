cd53171285ca8c99f5a3265fb2afbf2770561ecb
maxogod <maxi18wars@gmail.com>
2023-12-03 17:01:56 -0300

    added opponent name on top of the ingame board

--- truco-front/src/components/inGame/Ratings.tsx
+++ truco-front/src/components/inGame/Ratings.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { GameContext } from '../../context/gameContext';
+import { useContext } from 'react';
 
 const Ratings: React.FC = () => {
+
+  const { opponentName } = useContext(GameContext)
+
   return (
     <div>
-      <p className='my-14'>You (435) vs Athusss (478)</p>
+      <p className='my-14'>You (435) vs {`${opponentName}`} (478)</p>
     </div>
   );
 };
