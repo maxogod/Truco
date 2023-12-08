41e864072f0b382449f6fe7198107fd2f9ecc031
alenmonti <amonti@fi.uba.ar>
2023-12-07 21:24:15 -0300

    fix app

--- truco-front/src/App.tsx
+++ truco-front/src/App.tsx
@@ -8,19 +8,6 @@ function App() {
     <UserContextProvider>
       <GameContextProvider>
         <Router />
-        <div className='w-full h-[100vh] flex bg-background text-text'>
-          <NavBar />
-          <MainSideBar />
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/play" element={<InGame />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/leader-board" element={<LeaderBoard />} />
-            <Route path="/rules" element={<Rules />} />
-            <Route path="/login" element={<LogIn />} />
-            <Route path="/signup" element={<SignUp />} />
-          </Routes>
-        </div>
       </GameContextProvider>
     </UserContextProvider>
   )
