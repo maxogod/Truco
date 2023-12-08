a389c3c50dfb3be6bbbb8c6719b9741d0686e41a
alenmonti <amonti@fi.uba.ar>
2023-12-08 15:59:19 -0300

    logInIcon

--- truco-front/src/shared/NavBar.tsx
+++ truco-front/src/shared/NavBar.tsx
@@ -1,5 +1,5 @@
 import profile from '../assets/User_cicrle_duotone.png';
-import login from '../assets/login.png';
+import loginIcon from '../assets/logInIcon.png';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { UserContext } from '../context/userContext';
@@ -9,13 +9,13 @@ const NavBar: React.FC = () => {
   const { user } = useContext(UserContext)
 
   return (
-    <div className='absolute top-0 right-0 cursor-pointer z-20'>
+    <div className='absolute top-0 right-0 cursor-pointer z-20 m-2'>
       {user && <Link to='/profile'>
         <img src={profile} alt="" className='w-[60px] md:w-[70px] md:h-[70px]' />
       </Link>}
 
       {!user && <Link to="/login">
-        <img src={login} alt="" className='w-[70px] h-[70px] p-3 bg-[#6282a3da] rounded-full' />
+        <img src={loginIcon} alt="" className='w-[45px] h-[45px] m-1' />
       </Link>}
     </div>
   );
